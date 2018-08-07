29daa86c5e5c4f101a7e2ab1841d17ecfd0dac26
zhengluping <zhengluping@mylike.local>
2018-08-07 18:15:45 +0800

    更正test

--- app/src/main/java/com/mylike/his/activity/CustomerDetailsActivity.java
+++ app/src/main/java/com/mylike/his/activity/CustomerDetailsActivity.java
@@ -44,7 +44,7 @@ import butterknife.OnClick;
 
 /**
  * Created by zhengluping on 2018/1/24.
- * 客户详情aaa
+ * 客户详情
  */
 public class CustomerDetailsActivity extends BaseActivity {
     @Bind(R.id.webView)
